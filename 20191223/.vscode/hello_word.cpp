66dee318039d1490e1677c4c9710b862c6c11a53
zhumin94 <zhumin94@buaa.edu.cn>
2019-12-24 23:08:32 +0800

    代码20191224

---
+++ 20191223/.vscode/hello_word.cpp
@@ -0,0 +1,11 @@
+#include<iostream>  
+using namespace std;  
+int main( )  
+{  
+    int x;  
+    cin>>x;  
+    int y=x*x;
+    cout << "y=" << y << endl;
+    system("pause");
+    return 0;  
+}
