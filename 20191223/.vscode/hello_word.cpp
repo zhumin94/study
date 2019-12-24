84ea46f1c8dac2c5c9cc90757d9ee4aedfafc321
zhumin94 <zhumin94@buaa.edu.cn>
2019-12-24 23:50:53 +0800

    11111

--- 20191223/.vscode/hello_word.cpp
+++ 20191223/.vscode/hello_word.cpp
@@ -7,7 +7,8 @@ int main( )
     int y=x*x;
     cout << "y=" << y << endl;
     cout << "hello world" << endl;
-    cout << "git_rebase"  << endl;
+    cout << "git_rebase" << endl;
+    cout << "111111111"  << endl;
     system("pause");
     return 0;  
 }
